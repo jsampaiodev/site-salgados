43ee8eb8cebccbf4c5356b542e84eb838c4dd2d0
João Pedro <sampaiodepaulajoaopedro@gmail.com>
2026-08-17 16:50:52 -0300

    melhorias no JavaScript

--- script.js
+++ script.js
@@ -1,71 +1,35 @@
 console.log("O javascript esta funcionando! ");
 
-const botaoMaisCoxinha = document.getElementById("botao-mais-coxinha");
-
-const botaoMenosCoxinha = document.getElementById("botao-menos-coxinha");
-
-const quantidadeCoxinha=document.getElementById("quantidade-coxinha");
-
-
-
-botaoMaisCoxinha.addEventListener("click", function() {
-      quantidadeCoxinha.textContent = Number(quantidadeCoxinha.textContent) + 1;
-});
-
-botaoMenosCoxinha.addEventListener("click", function() {
-    if (Number(quantidadeCoxinha.textContent) > 0) {
-        quantidadeCoxinha.textContent = Number(quantidadeCoxinha.textContent) -1;
-}
-});
-
-const botaoMaisQuibe = document.getElementById("botao-mais-quibe");
-
-const botaoMenosQuibe = document.getElementById("botao-menos-quibe");
-
-const quantidadeQuibe=document.getElementById("quantidade-quibe");
-
-botaoMaisQuibe.addEventListener("click" , function(){
-    quantidadeQuibe.textContent= Number(quantidadeQuibe.textContent) + 1
-})
-
-botaoMenosQuibe.addEventListener("click", function(){
-    if (Number(quantidadeQuibe.textContent) > 0){
-        quantidadeQuibe.textContent = Number(quantidadeQuibe.textContent) -1;
-}
-});
-
-const botaoMaisPresuntoeQueijo = document.getElementById("botao-mais-presuntoequeijo");
-
-const botaoMenosPresuntoeQueijo = document.getElementById("botao-menos-presuntoequeijo");
-
-const quantidadePresuntoeQueijo=document.getElementById("quantidade-presuntoequeijo");
-
-botaoMaisPresuntoeQueijo.addEventListener("click" , function(){
-    quantidadePresuntoeQueijo.textContent= Number(quantidadePresuntoeQueijo.textContent) + 1
-})
-
-botaoMenosPresuntoeQueijo.addEventListener("click", function(){
-    if (Number(quantidadePresuntoeQueijo.textContent) > 0){
-        quantidadePresuntoeQueijo.textContent = Number(quantidadePresuntoeQueijo.textContent) -1;
-}
-});
-
-const botaoMaisEnroladinhodeSalsicha = document.getElementById("botao-mais-enroladinhodesalsicha");
-
-const botaoMenosEnroladinhodeSalsicha = document.getElementById("botao-menos-enroladinhodesalsicha");
-
-const quantidadeEnroladinhodeSalsicha=document.getElementById("quantidade-enroladinhodesalsicha");
-
-botaoMaisEnroladinhodeSalsicha.addEventListener("click" , function(){
-    quantidadeEnroladinhodeSalsicha.textContent= Number(quantidadeEnroladinhodeSalsicha.textContent) + 1
-})
-
-botaoMenosEnroladinhodeSalsicha.addEventListener("click", function(){
-    if (Number(quantidadeEnroladinhodeSalsicha.textContent) > 0){
-        quantidadeEnroladinhodeSalsicha.textContent = Number(quantidadeEnroladinhodeSalsicha.textContent) -1;
-}
-});
-
 const produtos = document.querySelectorAll(".produto");
 
-console.log(produtos);
+const total = document.getElementById("total");
+
+let valorTotal = 0;
+
+produtos.forEach(function(produto) {
+
+    const preco = 6;
+
+    const botaoMais = produto.querySelector(".botao-mais");
+
+    const botaoMenos = produto.querySelector(".botao-menos");
+
+    const quantidade = produto.querySelector(".quantidade");
+
+    botaoMais.addEventListener("click", function() {
+      quantidade.textContent = Number(quantidade.textContent) + 1;
+
+      const valorProduto = Number (quantidade.textContent) * preco;
+
+      total.textContent = valorProduto.toFixed(2).replace(".",",");
+    });
+
+
+    botaoMenos.addEventListener("click", function() {
+        if (Number(quantidade.textContent) > 0) {
+         quantidade.textContent = Number(quantidade.textContent) -1;
+        }
+    });
+});
+
+
